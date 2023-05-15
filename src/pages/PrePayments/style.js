c4c721b566c55b1aacc5f453db923602bbb4d592
Andre Oliveira <andreluisoliveira013@gmail.com>
2023-05-15 08:47:05 -0300

    fix: adjustments pre payment

--- src/pages/PrePayments/style.js
+++ src/pages/PrePayments/style.js
@@ -4,7 +4,7 @@ export const PrePaymentStyle = {
   TitleContainer: 'w-full h-auto mt-20 justify-between',
   TitleButtonContainer: 'w-16',
   ItemsBody: 'w-full h-auto mt-4 overflow-x-scroll',
-  ItemContainer: 'flex w-full h-auto rounded bg-[#F5F5FA] p-2 mt-2 rounded cursor-pointer',
+  ItemContainer: 'flex w-full h-auto min-h-[12vh] rounded bg-[#F5F5FA] p-2 mt-2 rounded cursor-pointer',
   ImageItemContainer: 'w-28 h-28 mr-10',
   InfoItemContainer: 'flex flex-col w-4/5 justify-between',
   ModalBodyContainer: 'flex h-auto',
